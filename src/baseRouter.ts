30dfd7c516f23d343b0f69023d12d90947d659b5
loic <loic.cledelin@student.junia.com>
2023-10-06 15:04:12 +0200

    update leonie

---
+++ src/baseRouter.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+export function createBaseRouter() {
+  const router = Router();
+
+  router.get("/", (_, res) => {
+    return res.json({ message: "I'm alive !" });
+  });
+
+  return router;
+}
---
+++ src/beerRouter.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+export function createBeerRouter() {
+  const router = Router();
+
+  router.get("/", (_, res) =>
+    res.json({
+      beers: [],
+    }),
+  );
+
+  return router;
+}
